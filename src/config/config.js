09bda75ae5dcfa400a70d4616150eb9230c3893b
SANJAY-1508 <psanjaykumar377@gmail.com>
2025-10-31 13:20:31 +0530

    `Switched API domain from production to local environment in config.js`

--- src/config/config.js
+++ src/config/config.js
@@ -1,4 +1,4 @@
-var API_DOMAIN = "https://gopuramfinanceweb.zentexus.com/api";
-//var API_DOMAIN = "http://localhost/gopuramfinanceweb/api";
+//var API_DOMAIN = "https://gopuramfinanceweb.zentexus.com/api";
+var API_DOMAIN = "http://localhost/gopuramfinanceweb/api";
 
 export default API_DOMAIN;
